6d380a75343125b79c9883f334c742b677984c34
meggs1 <megan.schertz@gmail.com>
2020-11-19 22:03:57 -0600

    commit tree 697f548671c454e620f817a8ed49f9eff83972d2

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -1,7 +1,14 @@
 require 'nokogiri'
 require 'open-uri'
 
-html = open("https://flatironschool.com/")
+doc = Nokogiri::HTML(open("http://flatironschool.com/"))
+courses = doc.css(".title-oE5vT4")
+
+# courses.each do |course|
+#     puts course.text.strip
+# end
+
+p doc.css(".title-oE5vT4")[0].children
 
 
 
